9d6f102149f17944bdc6c8e8c882647e13e8c3a4
Cheng2006 <37324931+Cheng2006@users.noreply.github.com>
2018-03-23 12:39:05 +0800

    Update 94. Binary Tree Inorder Traversal.java

--- Tree/94. Binary Tree Inorder Traversal.java
+++ Tree/94. Binary Tree Inorder Traversal.java
@@ -41,4 +41,32 @@ public class Solution {
         }
         return result;
     }
+    
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<Integer>();
+        if(root == null) {
+            return result;
+        }
+        TreeNode curr = root;
+        while(curr!=null){
+            if(curr.left==null){
+                result.add(curr.val);
+                curr = curr.right;
+            }else{
+                TreeNode prev = curr.left;
+                while(prev.right!=null && prev.right!=curr){
+                    prev = prev.right;
+                }
+                if(prev.right==null){
+                    prev.right = curr;
+                    curr = curr.left;
+                }else{
+                    prev.right = null;
+                    result.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+        return result;
+    }
 }
